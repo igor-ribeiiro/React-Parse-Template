f897e0f4309af203af3eeb2dfc489d8a3291f4c6
Samuel <samuelmtimbo@gmail.com>
2016-10-17 00:27:30 -0200

    init

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.jsx';
+
+console.log('hahah');
+
+ReactDOM.render(<App />, document.getElementById('app'));
